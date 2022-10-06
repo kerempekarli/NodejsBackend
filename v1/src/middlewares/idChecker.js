d9f42fe67c60abba53904a2c57d2b3580c084043
kerempekarli <kerempekarli@gmail.com>
2022-10-06 07:48:26 +0300

    Regex added

--- v1/src/middlewares/idChecker.js
+++ v1/src/middlewares/idChecker.js
@@ -4,7 +4,7 @@ const idChecker = (field) => (req,res,next) => {
 
     if(!req?.params[field || "id"]?.id?.match(/^[0-9a-fA-F]{24}$/)){
         return next(new ApiError("Lütfen geçerli bir ID bilgisi giriniz",400));
-         
+          
     }
     next()   
 }
